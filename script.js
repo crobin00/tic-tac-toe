285ba5d640e8a5837f43ccdba5ce9872535cac93
chris <christanr00@gmail.com>
2021-07-04 03:17:10 -0400

    added win/lose functionality and turn system

---
+++ script.js
@@ -0,0 +1,220 @@
+let winnerFound = false;
+const body = document.querySelector("body");
+const container = document.querySelector(".container");
+const announce = document.createElement("h1");
+const resetButton = document.createElement("button");
+const startButton = document.querySelector(".submit-button");
+const name1Div = document.querySelector(".name");
+const name2Div = document.querySelector(".name2");
+const inputtedName = document.getElementById("name");
+const inputtedName2 = document.getElementById("name2");
+const header = document.querySelector(".header");
+const player1Display = document.createElement("h3");
+const player2Display = document.createElement("h3");
+let turn = 0;
+
+const gameBoard = (() => {
+	const boardArray = ["", "", "", "", "", "", "", "", ""];
+
+	return { boardArray };
+})();
+
+const displayController = (() => {
+	const gameBoardSelect = document.querySelectorAll(".square");
+	const clickOnBoardEvent = gameBoardSelect.forEach((square) => {
+		square.addEventListener("click", (e) => {
+			console.log(e.target);
+			addPieceToBoard(square);
+			console.log(gameBoard.boardArray);
+			for (let i = 0; i < gameBoard.boardArray.length; i++) {
+				if (square.classList.contains(`square${i}`)) {
+					gameBoard.boardArray[i] = square.innerText;
+				}
+			}
+			if (game.calculateWin()) {
+				console.log("event listener removed");
+			}
+			console.log(gameBoard.boardArray);
+		});
+	});
+
+	const addPieceToBoard = (square) => {
+		if (square.innerText == "" && !winnerFound) {
+			square.innerText = game.calculateTurn();
+		}
+	};
+
+	const resetGame = (square) => {
+		console.log("resetted");
+		winnerFound = false;
+		for (let i = 0; i < gameBoard.boardArray.length; i++) {
+			gameBoard.boardArray[i] = "";
+		}
+		gameBoardSelect.forEach((square) => {
+			square.innerText = "";
+		});
+		announce.innerText = "";
+		resetButton.innerText = "";
+		player2Display.innerText = "";
+		player1Display.innerText = `${player1.name}'s turn`;
+		header.appendChild(player1Display);
+		turn = 1;
+	};
+
+	return { clickOnBoardEvent, resetGame };
+})();
+
+const game = (() => {
+	const calculateTurn = () => {
+		if (turn % 2 == 0) {
+			turn++;
+			player2Display.innerText = "";
+			player1Display.innerText = `${player1.name}'s turn`;
+			header.appendChild(player1Display);
+			return player1.piece;
+		} else {
+			turn++;
+			player1Display.innerText = "";
+			player2Display.innerText = `${player2.name}'s turn`;
+			header.appendChild(player2Display);
+			return player2.piece;
+		}
+	};
+
+	const calculateWin = () => {
+		if (
+			gameBoard.boardArray[0] == gameBoard.boardArray[1] &&
+			gameBoard.boardArray[1] == gameBoard.boardArray[2] &&
+			gameBoard.boardArray[0] != ""
+		) {
+			console.log("winner");
+			winnerFound = true;
+			player1Display.innerText = "";
+			player2Display.innerText = "";
+		}
+
+		if (
+			gameBoard.boardArray[3] == gameBoard.boardArray[4] &&
+			gameBoard.boardArray[4] == gameBoard.boardArray[5] &&
+			gameBoard.boardArray[3] != ""
+		) {
+			console.log("winner");
+			winnerFound = true;
+			player1Display.innerText = "";
+			player2Display.innerText = "";
+		}
+
+		if (
+			gameBoard.boardArray[6] == gameBoard.boardArray[7] &&
+			gameBoard.boardArray[7] == gameBoard.boardArray[8] &&
+			gameBoard.boardArray[6] != ""
+		) {
+			console.log("winner");
+			winnerFound = true;
+			player1Display.innerText = "";
+			player2Display.innerText = "";
+		}
+
+		if (
+			gameBoard.boardArray[0] == gameBoard.boardArray[3] &&
+			gameBoard.boardArray[3] == gameBoard.boardArray[6] &&
+			gameBoard.boardArray[0] != ""
+		) {
+			console.log("winner");
+			winnerFound = true;
+			player1Display.innerText = "";
+			player2Display.innerText = "";
+		}
+
+		if (
+			gameBoard.boardArray[1] == gameBoard.boardArray[4] &&
+			gameBoard.boardArray[4] == gameBoard.boardArray[7] &&
+			gameBoard.boardArray[1] != ""
+		) {
+			console.log("winner");
+			winnerFound = true;
+			player1Display.innerText = "";
+			player2Display.innerText = "";
+		}
+
+		if (
+			gameBoard.boardArray[2] == gameBoard.boardArray[5] &&
+			gameBoard.boardArray[5] == gameBoard.boardArray[8] &&
+			gameBoard.boardArray[2] != ""
+		) {
+			console.log("winner");
+			winnerFound = true;
+			player1Display.innerText = "";
+			player2Display.innerText = "";
+		}
+
+		if (
+			gameBoard.boardArray[0] == gameBoard.boardArray[4] &&
+			gameBoard.boardArray[4] == gameBoard.boardArray[8] &&
+			gameBoard.boardArray[0] != ""
+		) {
+			console.log("winner");
+			winnerFound = true;
+			player1Display.innerText = "";
+			player2Display.innerText = "";
+		}
+
+		if (
+			gameBoard.boardArray[2] == gameBoard.boardArray[4] &&
+			gameBoard.boardArray[4] == gameBoard.boardArray[6] &&
+			gameBoard.boardArray[2] != ""
+		) {
+			console.log("winner");
+			winnerFound = true;
+			player1Display.innerText = "";
+			player2Display.innerText = "";
+		}
+
+		if (!winnerFound && gameBoard.boardArray.every(checkDraw)) {
+			console.log("draw");
+			player1Display.innerText = "";
+			player2Display.innerText = "";
+			body.appendChild(announce);
+			body.appendChild(resetButton);
+			announce.innerText = "draw!";
+			resetButton.innerText = "reset game";
+		}
+
+		function checkDraw(index) {
+			return index != "";
+		}
+
+		if (winnerFound) {
+			body.appendChild(announce);
+			body.appendChild(resetButton);
+			announce.innerText = "WINNER FOUND!";
+			resetButton.innerText = "reset game";
+		}
+	};
+
+	return { calculateTurn, calculateWin };
+})();
+
+const Player = (name, piece) => {
+	return { name, piece };
+};
+
+let player1;
+let player2;
+
+resetButton.addEventListener("click", (e) => {
+	console.log("clciked btton");
+	displayController.resetGame();
+});
+
+startButton.onclick = function () {
+	player1 = Player(inputtedName.value, "X");
+	player2 = Player(inputtedName2.value, "O");
+	container.classList.remove("hidden");
+	name1Div.classList.add("hidden");
+	name2Div.classList.add("hidden");
+	player1Display.innerText = `${player1.name}'s turn`;
+	header.appendChild(player1Display);
+	turn++;
+	console.log(player1.name);
+};
