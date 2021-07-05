c05cab0670220c2f5ebf9c4cd87af8dc30f10df8
chris <christanr00@gmail.com>
2021-07-04 21:54:36 -0400

    added functionality to add names, hover effects, and better styling

--- script.js
+++ script.js
@@ -2,7 +2,12 @@ let winnerFound = false;
 const body = document.querySelector("body");
 const container = document.querySelector(".container");
 const announce = document.createElement("h1");
+announce.style.marginTop = "30px";
 const resetButton = document.createElement("button");
+resetButton.style.width = "110px";
+resetButton.style.height = "30px";
+resetButton.style.background = "#3C8DAD";
+resetButton.style.borderRadius = "3px";
 const startButton = document.querySelector(".submit-button");
 const name1Div = document.querySelector(".name");
 const name2Div = document.querySelector(".name2");
@@ -11,6 +16,7 @@ const inputtedName2 = document.getElementById("name2");
 const header = document.querySelector(".header");
 const player1Display = document.createElement("h3");
 const player2Display = document.createElement("h3");
+let winningPlayer;
 let turn = 0;
 
 const gameBoard = (() => {
@@ -23,18 +29,14 @@ const displayController = (() => {
 	const gameBoardSelect = document.querySelectorAll(".square");
 	const clickOnBoardEvent = gameBoardSelect.forEach((square) => {
 		square.addEventListener("click", (e) => {
-			console.log(e.target);
 			addPieceToBoard(square);
-			console.log(gameBoard.boardArray);
 			for (let i = 0; i < gameBoard.boardArray.length; i++) {
 				if (square.classList.contains(`square${i}`)) {
 					gameBoard.boardArray[i] = square.innerText;
 				}
 			}
 			if (game.calculateWin()) {
-				console.log("event listener removed");
 			}
-			console.log(gameBoard.boardArray);
 		});
 	});
 
@@ -45,7 +47,6 @@ const displayController = (() => {
 	};
 
 	const resetGame = (square) => {
-		console.log("resetted");
 		winnerFound = false;
 		for (let i = 0; i < gameBoard.boardArray.length; i++) {
 			gameBoard.boardArray[i] = "";
@@ -59,6 +60,10 @@ const displayController = (() => {
 		player1Display.innerText = `${player1.name}'s turn`;
 		header.appendChild(player1Display);
 		turn = 1;
+		resetButton.style.background = "#dddddd";
+		resetButton.style.width = "0";
+		resetButton.style.height = "0";
+		resetButton.disabled = true;
 	};
 
 	return { clickOnBoardEvent, resetGame };
@@ -87,10 +92,20 @@ const game = (() => {
 			gameBoard.boardArray[1] == gameBoard.boardArray[2] &&
 			gameBoard.boardArray[0] != ""
 		) {
-			console.log("winner");
+			if (gameBoard.boardArray[0] == "X") {
+				winningPlayer = player1.name;
+			} else {
+				winningPlayer = player2.name;
+			}
+
 			winnerFound = true;
 			player1Display.innerText = "";
 			player2Display.innerText = "";
+			resetButton.disabled = false;
+			resetButton.style.width = "110px";
+			resetButton.style.height = "30px";
+			resetButton.style.background = "#3C8DAD";
+			resetButton.style.borderRadius = "3px";
 		}
 
 		if (
@@ -98,10 +113,20 @@ const game = (() => {
 			gameBoard.boardArray[4] == gameBoard.boardArray[5] &&
 			gameBoard.boardArray[3] != ""
 		) {
-			console.log("winner");
+			if (gameBoard.boardArray[3] == "X") {
+				winningPlayer = player1.name;
+			} else {
+				winningPlayer = player2.name;
+			}
+
 			winnerFound = true;
 			player1Display.innerText = "";
 			player2Display.innerText = "";
+			resetButton.disabled = false;
+			resetButton.style.width = "110px";
+			resetButton.style.height = "30px";
+			resetButton.style.background = "#3C8DAD";
+			resetButton.style.borderRadius = "3px";
 		}
 
 		if (
@@ -109,10 +134,20 @@ const game = (() => {
 			gameBoard.boardArray[7] == gameBoard.boardArray[8] &&
 			gameBoard.boardArray[6] != ""
 		) {
-			console.log("winner");
+			if (gameBoard.boardArray[6] == "X") {
+				winningPlayer = player1.name;
+			} else {
+				winningPlayer = player2.name;
+			}
+
 			winnerFound = true;
 			player1Display.innerText = "";
 			player2Display.innerText = "";
+			resetButton.disabled = false;
+			resetButton.style.width = "110px";
+			resetButton.style.height = "30px";
+			resetButton.style.background = "#3C8DAD";
+			resetButton.style.borderRadius = "3px";
 		}
 
 		if (
@@ -120,10 +155,20 @@ const game = (() => {
 			gameBoard.boardArray[3] == gameBoard.boardArray[6] &&
 			gameBoard.boardArray[0] != ""
 		) {
-			console.log("winner");
+			if (gameBoard.boardArray[0] == "X") {
+				winningPlayer = player1.name;
+			} else {
+				winningPlayer = player2.name;
+			}
+
 			winnerFound = true;
 			player1Display.innerText = "";
 			player2Display.innerText = "";
+			resetButton.disabled = false;
+			resetButton.style.width = "110px";
+			resetButton.style.height = "30px";
+			resetButton.style.background = "#3C8DAD";
+			resetButton.style.borderRadius = "3px";
 		}
 
 		if (
@@ -131,10 +176,20 @@ const game = (() => {
 			gameBoard.boardArray[4] == gameBoard.boardArray[7] &&
 			gameBoard.boardArray[1] != ""
 		) {
-			console.log("winner");
+			if (gameBoard.boardArray[1] == "X") {
+				winningPlayer = player1.name;
+			} else {
+				winningPlayer = player2.name;
+			}
+
 			winnerFound = true;
 			player1Display.innerText = "";
 			player2Display.innerText = "";
+			resetButton.disabled = false;
+			resetButton.style.width = "110px";
+			resetButton.style.height = "30px";
+			resetButton.style.background = "#3C8DAD";
+			resetButton.style.borderRadius = "3px";
 		}
 
 		if (
@@ -142,10 +197,20 @@ const game = (() => {
 			gameBoard.boardArray[5] == gameBoard.boardArray[8] &&
 			gameBoard.boardArray[2] != ""
 		) {
-			console.log("winner");
+			if (gameBoard.boardArray[2] == "X") {
+				winningPlayer = player1.name;
+			} else {
+				winningPlayer = player2.name;
+			}
+
 			winnerFound = true;
 			player1Display.innerText = "";
 			player2Display.innerText = "";
+			resetButton.disabled = false;
+			resetButton.style.width = "110px";
+			resetButton.style.height = "30px";
+			resetButton.style.background = "#3C8DAD";
+			resetButton.style.borderRadius = "3px";
 		}
 
 		if (
@@ -153,10 +218,20 @@ const game = (() => {
 			gameBoard.boardArray[4] == gameBoard.boardArray[8] &&
 			gameBoard.boardArray[0] != ""
 		) {
-			console.log("winner");
+			if (gameBoard.boardArray[0] == "X") {
+				winningPlayer = player1.name;
+			} else {
+				winningPlayer = player2.name;
+			}
+
 			winnerFound = true;
 			player1Display.innerText = "";
 			player2Display.innerText = "";
+			resetButton.disabled = false;
+			resetButton.style.width = "110px";
+			resetButton.style.height = "30px";
+			resetButton.style.background = "#3C8DAD";
+			resetButton.style.borderRadius = "3px";
 		}
 
 		if (
@@ -164,20 +239,34 @@ const game = (() => {
 			gameBoard.boardArray[4] == gameBoard.boardArray[6] &&
 			gameBoard.boardArray[2] != ""
 		) {
-			console.log("winner");
+			if (gameBoard.boardArray[2] == "X") {
+				winningPlayer = player1.name;
+			} else {
+				winningPlayer = player2.name;
+			}
+
 			winnerFound = true;
 			player1Display.innerText = "";
 			player2Display.innerText = "";
+			resetButton.disabled = false;
+			resetButton.style.width = "110px";
+			resetButton.style.height = "30px";
+			resetButton.style.background = "#3C8DAD";
+			resetButton.style.borderRadius = "3px";
 		}
 
 		if (!winnerFound && gameBoard.boardArray.every(checkDraw)) {
-			console.log("draw");
 			player1Display.innerText = "";
 			player2Display.innerText = "";
 			body.appendChild(announce);
 			body.appendChild(resetButton);
-			announce.innerText = "draw!";
-			resetButton.innerText = "reset game";
+			announce.innerText = "Draw!";
+			resetButton.innerText = "Reset game";
+			resetButton.disabled = false;
+			resetButton.style.width = "110px";
+			resetButton.style.height = "30px";
+			resetButton.style.background = "#3C8DAD";
+			resetButton.style.borderRadius = "3px";
 		}
 
 		function checkDraw(index) {
@@ -187,8 +276,12 @@ const game = (() => {
 		if (winnerFound) {
 			body.appendChild(announce);
 			body.appendChild(resetButton);
-			announce.innerText = "WINNER FOUND!";
-			resetButton.innerText = "reset game";
+			if (winningPlayer == player1.name) {
+				announce.innerText = `${player1.name} won!`;
+			} else {
+				announce.innerText = `${player2.name} won!`;
+			}
+			resetButton.innerText = "Reset game";
 		}
 	};
 
@@ -203,13 +296,12 @@ let player1;
 let player2;
 
 resetButton.addEventListener("click", (e) => {
-	console.log("clciked btton");
 	displayController.resetGame();
 });
 
 startButton.onclick = function () {
-	player1 = Player(inputtedName.value, "X");
-	player2 = Player(inputtedName2.value, "O");
+	player1 = Player(inputtedName.value, "O");
+	player2 = Player(inputtedName2.value, "X");
 	container.classList.remove("hidden");
 	name1Div.classList.add("hidden");
 	name2Div.classList.add("hidden");
